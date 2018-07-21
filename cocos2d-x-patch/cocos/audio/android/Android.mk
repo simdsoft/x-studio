4948f9feb23668c96bead4817e6125839d6d8d3d
halx99 <halx99@163.com>
2018-07-22 01:28:19 +0800

    [ANDROID] Refactor audio module, upgrade dragonBones to 5.5, autolink dragonBones

---
+++ cocos2d-x-patch/cocos/audio/android/Android.mk
@@ -0,0 +1,41 @@
+LOCAL_PATH := $(call my-dir)
+
+#New AudioEngine
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := audioengine_static
+
+LOCAL_MODULE_FILENAME := libaudioengine
+
+LOCAL_SRC_FILES := AudioEngineImpl.cpp \
+                   ../AudioEngine.cpp \
+                   AudioDecoder.cpp \
+                   AudioDecoderOgg.cpp \
+                   AudioDecoderMp3.cpp \
+                   AudioCache.cpp \
+                   AudioDecoderManager.cpp \
+                   AudioPlayer.cpp \
+                   cddSimpleAudioEngine.cpp \
+                   ccdandroidUtils.cpp \
+                   jni/cddandroidAndroidJavaEngine.cpp \
+                   utils/Utils.cpp
+
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include
+
+LOCAL_EXPORT_LDLIBS := -lOpenSLES
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include \
+                    $(LOCAL_PATH)/../.. \
+                    $(LOCAL_PATH)/../../platform/android \
+                    $(LOCAL_PATH)/../../../external/android-specific/OggDecoder/include \
+                    $(LOCAL_PATH)/../../../external/android-specific/MP3Decoder/include \
+                    $(LOCAL_PATH)/../../../external/android-specific/OpenalSoft/include
+
+LOCAL_SHARED_LIBRARIES += mpg123_shared openal_shared
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_ogg_static cocos_vorbis_static cocos_vorbisfile_static
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,android-specific/OggDecoder/prebuilt)
+$(call import-module,android-specific/MP3Decoder/prebuilt)
+$(call import-module,android-specific/OpenalSoft/prebuilt)
