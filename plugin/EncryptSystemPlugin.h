3cb4431ebe429f620b260381bf4b1b90b295771e
HALX99 <halx99@live.com>
2020-07-23 11:01:41 +0800

    Update EncryptSystemPlugin.h

--- plugin/EncryptSystemPlugin.h
+++ plugin/EncryptSystemPlugin.h
@@ -8,17 +8,34 @@
 // only under terms and conditions
 // of the accompanying license agreement.
 //*******************************************************************************
-//
+// Version: 10.0.9000.21
 /*
 The x-sutdio Pro EncryptSystemPlugin interface
 The plugin should export API: "XSPESSIoControl"
 */
 #pragma once
 
+/// <summary>
+/// The input control code
+/// </summary>
 enum {
-    ESSCC_PREENCRYPT = 1,
-    ESSCC_COMPUTE_FILE_CONTENT_HASH,
-    ESSCC_COMPUTE_FILE_NAME_HASH,
+    ES_ICC_PREENCRYPT = 1,
+    ES_ICC_COMPUTE_FILE_CONTENT_HASH,
+    ES_ICC_COMPUTE_FILE_NAME_HASH,
 };
 
+// The output control code, returned by the XSPESSIoControl
+// when <= 0
+// since: 10.0.9000.21
+enum
+{
+    ES_OCC_DEFAULT = 0,
+    ES_OCC_SKIP = -1001, // only works for ES_ICC_PREENCRYPT
+};
+
+/// <summary>
+/// @returns:
+///    > 0: the actual bytes have been write to outputBuffer
+///   <= 0: Output Control Code: ES_OCC_DEFAULT or ES_OCC_SKIP
+/// </summary>
 typedef int (*PFNXSPESSIOCONTROL)(int controlCode, const void* lpInBuffer, int nInBufferSize, void* lpOutBuffer, int nOutBufferSize);
